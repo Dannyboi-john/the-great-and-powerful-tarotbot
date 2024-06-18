f25dbb6bad93ab417506c820ab1e72d96501092a
danny <ddjjpp340@gmail.com>
2024-06-18 17:23:07 -0230

    Added submit button and some CSS styling

--- the-great-and-powerful-tarotbot/src/App.jsx
+++ the-great-and-powerful-tarotbot/src/App.jsx
@@ -7,8 +7,19 @@ function App() {
   return (
     <>
       <h1>The Great and Powerful TarotBot</h1>
+      <textarea className="query-text" 
+        placeholder="Compose Query Here" 
+        name="query" 
+        rows={5} 
+        cols={80} />
 
-      <textarea placeholder="Compose Query Here" name="query" rows={5} cols={80} />
+        <br />
+
+      <button 
+        className="submit-button"
+        name="submit"
+        >Submit Query
+      </button>
     </>
   )
 }
