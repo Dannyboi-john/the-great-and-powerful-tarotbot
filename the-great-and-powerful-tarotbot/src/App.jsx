b2e31678a13a4d5a254e61284165243714f5abec
danny <ddjjpp340@gmail.com>
2024-06-19 18:51:35 -0230

    Added setQuery to onChange in textarea

--- the-great-and-powerful-tarotbot/src/App.jsx
+++ the-great-and-powerful-tarotbot/src/App.jsx
@@ -2,18 +2,28 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [query, setQuery] = useState();
+
   return (
     <>
       <h1>The Great and Powerful TarotBot</h1>
+      <h2>{query}</h2>
       <textarea className="query-text" 
         placeholder="Compose Query Here" 
         name="query" 
         rows={5} 
-        cols={80} />
+        cols={80}
+        onChange={
+          e => setQuery(e.target.value)
+        } />
 
         <br />
 
       <button 
+        onClick={
+          e => {}
+        }
         className="submit-button"
         name="submit"
         >
