4bf0b182528b88cced6eefbffa0873bc32c5c51a
danny <ddjjpp340@gmail.com>
2024-06-21 22:32:20 -0230

    Created Landing.jsx and imported it to App.jsx

--- the-great-and-powerful-tarotbot/src/App.jsx
+++ the-great-and-powerful-tarotbot/src/App.jsx
@@ -1,65 +1,12 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
+import Landing from './Landing.jsx'
 
 function App() {
 
-  const [query, setQuery] = useState();
-
-  function handleSubmit(e) {
-
-    // Prevents browser from reloading the page.
-    e.preventDefault();
-
-    // Read the form data
-    const form = e.target;
-    const formData = new FormData(form);
-
-    const formJson = Object.fromEntries(formData.entries());
-    console.log(formJson);
-
-  }
-
   return (
-    <>
-      <h1>The Great and Powerful TarotBot</h1>
-      
-      <form onSubmit={handleSubmit}>
-        <textarea className="query-text" 
-          placeholder="Compose Query Here" 
-          name="query" 
-          rows={5} 
-          cols={80}
-          onChange={
-            e => setQuery(e.target.value)
-          } />
-
-          <br />
-
-        <button 
-          onClick={
-            e => {}
-          }
-          className="submit-button"
-          name="submit"
-          >
-            <span
-              className="front"
-              type="submit">Submit Query</span>
-        </button>
-      </form>
-    </>
+      <Landing />
   )
 }
-
-/* Inspo
-generateTarotCards() {
-  let tarotCards = [] // empty arr
-  for (3 times)
-     <TarotCard name=... desc=...>
- return (
-        tarotCards
-         
- )
-} */
 
 export default App
---
+++ the-great-and-powerful-tarotbot/src/Landing.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function Landing() {
+
+  const [query, setQuery] = useState();
+
+  function handleSubmit(e) {
+
+    // Prevents browser from reloading the page.
+    e.preventDefault();
+
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
+
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+
+  }
+
+  return (
+    <>
+      <h1>The Great and Powerful TarotBot</h1>
+      <h2>{query}</h2>
+      
+      <form onSubmit={handleSubmit}>
+        <textarea className="query-text" 
+          placeholder="Compose Query Here" 
+          name="query" 
+          rows={5} 
+          cols={80}
+          onChange={
+            e => setQuery(e.target.value)
+          }
+          />
+
+          <br />
+
+        <button 
+          className="submit-button"
+          name="submit"
+          >
+            <span
+              className="front"
+              type="submit">Submit Query</span>
+        </button>
+      </form>
+    </>
+  )
+}
+
+/* Inspo
+generateTarotCards() {
+  let tarotCards = [] // empty arr
+  for (3 times)
+     <TarotCard name=... desc=...>
+ return (
+        tarotCards
+         
+ )
+} */
+
+export default Landing
