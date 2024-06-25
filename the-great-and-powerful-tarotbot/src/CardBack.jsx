c6318feb093b16e276b5df9c67d5fd2b0f376e49
danny <ddjjpp340@gmail.com>
2024-06-25 01:25:58 -0230

    Added 3 face-down tarot cards after query is submitted

--- the-great-and-powerful-tarotbot/src/CardBack.jsx
+++ the-great-and-powerful-tarotbot/src/CardBack.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './App.css'
+import tarotBack from './assets/tarot-back.jpg'
 
 function CardBack(props) {
     return (
-        <div>Test</div>
+        <>
+            <img className="tarot-back" src={tarotBack} atl="Back of a tarot card"/>
+            <img className="tarot-back" src={tarotBack} atl="Back of a tarot card"/>
+            <img className="tarot-back" src={tarotBack} atl="Back of a tarot card"/>
+        </>
     )
     
 }
