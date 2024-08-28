cb0cdfa7f8743c32665150a27b8326957d2d3906
danny <ddjjpp340@gmail.com>
2024-08-28 14:25:39 -0230

    Deleted a few comments / cleaned up code

--- the-great-and-powerful-tarotbot/src/CardModal.jsx
+++ the-great-and-powerful-tarotbot/src/CardModal.jsx
@@ -1,26 +1,9 @@
 import React, { useEffect, useRef } from "react";
-// import { Children } from "react";
 
-function CardModal({ openModal, closeModal,  children, /* delay */}) {
+
+function CardModal({ openModal, closeModal,  children }) {
     const ref = useRef();
 
-    // Handles the typing animation.
-/*     const [currentText, setCurrentText] = useState('');
-    const [currentIndex, setCurrentIndex] = useState(0); */
-
-
-/*     // Using setTimeout to animate text on card flip
-    useEffect(()=> {
-        if (currentIndex < children.length) {
-            const timeout = setTimeout(() => {
-                setCurrentText(prevText => prevText + children[currentIndex]);
-                setCurrentIndex(prevIndex => prevIndex + 1);
-            }, delay);
-
-            return () => clearTimeout(timeout);
-        }
-    }, [currentIndex, delay, children]);
- */
 
     useEffect(() => {
         if (openModal) {
