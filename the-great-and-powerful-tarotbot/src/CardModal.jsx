c6c599d9e46ac3fdb29343d636df8435db00afec
danny <ddjjpp340@gmail.com>
2024-09-04 14:59:43 -0230

    Changed the close button on modal to X icon

--- the-great-and-powerful-tarotbot/src/CardModal.jsx
+++ the-great-and-powerful-tarotbot/src/CardModal.jsx
@@ -13,6 +13,11 @@ function CardModal({ openModal, closeModal,  children }) {
         }
     }, [openModal]);
 
+    const closeSVG = <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" fill="currentColor" className="bi bi-x-circle" viewBox="0 0 16 16">
+    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+  </svg>
+
     return (
         <>
         <dialog
@@ -20,18 +25,22 @@ function CardModal({ openModal, closeModal,  children }) {
             onCancel={closeModal}
             className="description-modal"
         >
+        <div 
+            className="close-icon"
+            onClick={closeModal}>
+            {closeSVG}
+        </div>
         {children}
-            <br/>
-            <div
+{/*             <div
                 className="close-button-container"
                 >
-                <button 
+                <div 
                     onClick={closeModal}
                     className="close-button"
                 >
-                    Close
-            </button>
-            </div>
+                    <span className="close-icon">{closeSVG}</span>
+                </div>
+            </div> */}
         </dialog>
         </>
     )
