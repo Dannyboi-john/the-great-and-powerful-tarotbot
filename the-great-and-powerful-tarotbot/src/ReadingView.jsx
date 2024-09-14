28f37211ee3cde4f3b993bec311c73057663963b
danny <ddjjpp340@gmail.com>
2024-09-14 09:52:30 -0230

    ReadingView is now semi-operational

--- the-great-and-powerful-tarotbot/src/ReadingView.jsx
+++ the-great-and-powerful-tarotbot/src/ReadingView.jsx
@@ -1,13 +1,33 @@
 import React, { useContext } from 'react';
+import { DataContext } from './DataContext'
+import Card from './Card'
 
 function ReadingView() {
+
+    const { data } = useContext(DataContext);
 
 
 
 
     return (
         <>
-        <div>THIS IS JUST A TEST TO SEE IF IT WORKED :D</div>
+        <div className="reading-container">
+            <div className="reading-past-card">
+            <Card 
+                cardData={data.cards[0]}
+                />
+            </div>
+            <div className="reading-present card">
+                <Card 
+                    cardData={data.cards[1]}
+                    />
+            </div>
+            <div className="reading-future-card">
+                <Card 
+                    cardData={data.cards[2]}
+                    />
+            </div>
+        </div>
         </>
     )
 }
